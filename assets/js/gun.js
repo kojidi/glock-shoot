5e1caee0c6763f4757cd9dde6e936f5192eba0b8
Hamid Rajabi Kojidi <rajabihamid75@gmail.com>
2024-06-03 17:11:03 +0330

    initial commit

---
+++ assets/js/gun.js
@@ -0,0 +1,27 @@
+const gun = document.querySelector('.gun-box');
+// const bullet = document.querySelector('.bullet');
+
+const centerX = window.innerWidth / 2;
+const centerY = (window.innerHeight) / 2;
+
+let bullet = document.querySelector('.bullet');
+
+document.addEventListener('mousemove', e => {
+    let x = e.pageX;
+    let y = e.pageY;
+    angle = Math.atan2(y - centerY, x - centerX) * 180 / Math.PI;
+    gun.style.transform = `rotate(${angle}deg)`; 
+})
+
+document.addEventListener('click', () => {
+    bullet.style.right = "-10000px";
+    
+    const newbullet = document.createElement('img');
+    newbullet.classList.add('bullet');
+    newbullet.src = "./assets/imgs/bullet.png";
+    newbullet.alt = "The Bullet";
+    gun.prepend(newbullet);
+    
+    bullet = document.querySelector('.bullet');
+});
+
